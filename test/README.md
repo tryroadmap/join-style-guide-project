836bc9461828f91cc79d463d75b066cb292a4d49
Hoofar Pourzand <support@lotustech.io>
2019-04-15 15:37:24 -0400

    for interview tests

---
+++ test/README.md
@@ -0,0 +1,4 @@
+### How to use the scripts for interviews
+```
+from load_basic_var import atuple
+```
---
+++ test/load_basic_var.py
@@ -0,0 +1,9 @@
+### Loads in alist, atuple, astring
+alist =['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+
+
+astring = "NACA0012H for VAWT from Sandia report SAND80-2114"
+atuple =  (
+(1.00000,	 0.00126),
+(0.95000,	 0.00807)
+)
